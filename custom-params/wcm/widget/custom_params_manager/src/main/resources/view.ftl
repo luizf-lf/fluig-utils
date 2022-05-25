28eb9eb450ac6e49d2f243f764e9667843c518ed
Luiz Ferreira <luizfernando_lf@hotmail.com.br>
2022-05-25 16:38:46 -0300

    feat: dataset table render implemented + translations added

--- custom-params/wcm/widget/custom_params_manager/src/main/resources/view.ftl
+++ custom-params/wcm/widget/custom_params_manager/src/main/resources/view.ftl
@@ -5,14 +5,51 @@
 >
   <ol class="breadcrumb">
     <li><a href="/portal" class="flaticon flaticon-home icon-sm" title="Home"></a></li>
-    <li>Customizações</li>
-    <li class="active">Configurações De Parâmetros</li>
+    <li>${i18n.getTranslation("navigation.customizations")}</li>
+    <li class="active">${i18n.getTranslation("navigation.active")}</li>
   </ol>
   <div class="page-header">
-    <h2>Configurações De Parâmetros</h2>
+    <h2>${i18n.getTranslation("application.header")}</h2>
   </div>
-  <div id="custom_params_manager_container_${instanceId}">
-    
+  <div id="custom_params_manager_${instanceId}_content">
+    <div class="row fs-md-margin-bottom">
+      <div class="col-md-8">
+        <button class="btn btn-primary" id="btnAddRecords_${instanceId}" data-add-record>
+          <div class="button-icon-aligned">
+            <i class="fluigicon fluigicon-fileadd icon-sm fs-sm-margin-right"></i>
+            ${i18n.getTranslation("buttons.include")}
+          </div>
+        </button>
+        <button class="btn btn-default" id="btnEditRecords_${instanceId}" data-edit-record disabled>
+          <div class="button-icon-aligned">
+            <i class="fluigicon fluigicon-pencil icon-sm fs-sm-margin-right"></i>
+            ${i18n.getTranslation("buttons.edit")}
+          </div>
+        </button>
+        <button class="btn btn-default" id="btnDeleteRecords_${instanceId}" data-delete-record disabled>
+          <div class="button-icon-aligned">
+            <i class="fluigicon fluigicon-trash icon-sm fs-sm-margin-right"></i>
+            ${i18n.getTranslation("buttons.delete")}
+          </div>
+        </button>
+      </div>
+      <div class="col-md-4">
+        <div class="input-group">
+          <input class="form-control" id="inputSearchRecords_${instanceId}" type="text" placeholder="${i18n.getTranslation('search.placeholder')}" data-input-search>
+          <div class="input-group-addon">
+              <span class="fluigicon fluigicon-search"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div id="dataTableContainer_${instanceId}">
+          <#--  Datatable vem agui  -->
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
--- custom-params/wcm/widget/custom_params_manager/src/main/webapp/resources/js/custom_params_manager.js
+++ custom-params/wcm/widget/custom_params_manager/src/main/webapp/resources/js/custom_params_manager.js
@@ -1,6 +1,7 @@
 var custom_params_manager = SuperWidget.extend({
   instanceId: null,
   loaderInstance: null,
+  datasetValues: null,
 
   formId: null,
   formDataset: null,
@@ -8,19 +9,46 @@ var custom_params_manager = SuperWidget.extend({
 
   translations: {
     pt_BR: {
+      instanceErrorTitle: 'Erro ao instanciar a widget.',
+      instanceError: 'Houve um erro ao instanciar a widget, veja detalhes:',
       saveSettingsSuccess: 'Configurações salvas com sucesso.',
       saveSettingsError: 'Erro ao salvar as configurações.',
       selectForm: 'Selecione o formulário',
+      noParams: 'Nenhum parâmetro cadastrado',
+      noParamsFound: 'Nenhum parâmetro encontrado',
+      noParamsAlt: 'Nenhum registro',
+      nonexistentDataset: 'Dataset informado não existe. Verifique nas configurações da Widget se o dataset informado está vinculado ao formulário vinculado.',
+      tableHeadId: 'Identificador',
+      tableHeadVal: 'Valor',
+      tableHeadDesc: 'Descrição',
     },
     es: {
+      instanceErrorTitle: 'Error al crear la widget.',
+      instanceError: 'Hubo un error al crear una instancia del widget, ver detalles:',
       saveSettingsSuccess: 'Configuración guardada con éxito.',
       saveSettingsError: 'Error al guardar la configuración.',
       selectForm: 'Seleciona lo formulário',
+      noParams: 'Ningún parámetro registrado',
+      noParamsFound: 'Ningún parámetro encontrado',
+      noParamsAlt: 'Ningún parámetro',
+      nonexistentDataset: 'El conjunto de datos informado no existe. Compruebe en la configuración del widget si el conjunto de datos informado está vinculado al formulario vinculado.',
+      tableHeadId: 'Identificador',
+      tableHeadVal: 'Valor',
+      tableHeadDesc: 'Descripción',
     },
     en_US: {
+      instanceErrorTitle: 'Error while instancing the widget.',
+      instanceError: 'An error occurred while instancing the widget, see details:',
       saveSettingsSuccess: 'Settings saved successfully',
       saveSettingsError: 'Error while saving settings',
       selectForm: 'Select form',
+      noParams: 'No parameters registered',
+      noParamsFound: 'No parameters found',
+      noParamsAlt: 'No records',
+      nonexistentDataset: 'Dataset informed does not exist. Check in the Widget settings if the informed dataset is linked to the linked form.',
+      tableHeadId: 'Identifier',
+      tableHeadVal: 'Value',
+      tableHeadDesc: 'Description',
     },
   },
 
@@ -31,7 +59,7 @@ var custom_params_manager = SuperWidget.extend({
    * @returns {string} the translated string
    */
   getTranslation: function (translationId) {
-    var userLocale = ['pt_BR', 'es', 'en_US'].includes(WCMAPI.getLocale())
+    const userLocale = ['pt_BR', 'es', 'en_US'].includes(WCMAPI.getLocale())
       ? WCMAPI.getLocale()
       : 'en_US';
     return this.translations[userLocale][translationId];
@@ -43,14 +71,14 @@ var custom_params_manager = SuperWidget.extend({
    * @since 2022/05/17
    */
   saveSettings: function () {
-    var instanceId = this.instanceId;
-    var args = {
+    const instanceId = this.instanceId;
+    const args = {
       formId: $('#inputFormId_' + instanceId).val(),
       formDataset: $('#inputFormDs_' + instanceId).val(),
       removesTittle: $('#checkRemoveTitle_' + instanceId).is(':checked'),
     };
 
-    var result = WCMSpaceAPI.PageService.UPDATEPREFERENCES(
+    const result = WCMSpaceAPI.PageService.UPDATEPREFERENCES(
       { async: false },
       instanceId,
       args
@@ -79,9 +107,9 @@ var custom_params_manager = SuperWidget.extend({
    */
   loadFormData: async function (documentId) {
     this.loaderInstance.show();
-    var instanceId = this.instanceId;
+    const instanceId = this.instanceId;
     $('#inputFormId_' + instanceId).val(documentId);
-    var dsDocument = await DatasetFactory.getDataset(
+    const dsDocument = await DatasetFactory.getDataset(
       'document',
       ['datasetName'],
       [
@@ -105,7 +133,7 @@ var custom_params_manager = SuperWidget.extend({
    * @since 2022/05/18
    */
   selectDocument: function () {
-    var that = this;
+    const that = this;
     ECMBC.searchDocument(
       {
         title: this.getTranslation('selectForm'),
@@ -127,7 +155,7 @@ var custom_params_manager = SuperWidget.extend({
    * @since 2022/05/18
    */
   loadSavedSettings: function () {
-    var instanceId = this.instanceId;
+    const instanceId = this.instanceId;
 
     if (this.formId != null) {
       $('#inputFormId_' + instanceId).val(this.formId);
@@ -143,25 +171,162 @@ var custom_params_manager = SuperWidget.extend({
   },
 
   /**
+   * @method renderTable
+   * @description method that renders the dataset values table
+   * @param {object[]} data array containing the dataset values
+   * @since 2022/05/25
+   */
+   renderTable: function (data) {
+    const instanceId = this.instanceId;
+    const htmlContainer = '#dataTableContainer_' + instanceId;
+
+    $(htmlContainer).html(`
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th><input type="checkbox" data-check-all id="checkToggleAll_' + instanceId + '" data-toggle-action-buttons /></th>
+            <th>Id</th>
+            <th>Identificador</th>
+            <th>Valor</th>
+            <th>Descrição</th>
+          </tr>
+        </thead>
+        <tbody>
+        ${data.map((item) => `
+            <tr id="record_${item.documentid}">
+              <td>
+                <input type="checkbox" id="${item.documentid}" class="datatable-check-option" data-toggle-action-buttons>
+              </td>
+              <td>
+                ${item.documentid}
+              </td>
+              <td>
+                ${item.paramID}
+              </td>
+              <td>
+                ${item.paramValueIsSensitive == 'on' ? "*".repeat(15) : item.paramValue}
+              </td>
+              <td>
+                ${item.paramDescription}
+              </td>
+            </tr>
+          `).join('\n')}
+        </tbody>
+      </table>
+    `);
+
+    if (data.length == 0) {
+      $(htmlContainer).append(
+        `<div class="alert alert-warning">${this.getTranslation('noParamsFound')}</div>`
+      );
+    }
+  },
+    
+  /**
+   * @method loadDataset
+   * @description loads the form params dataset
+   * @returns {DatasetDTO | null} fluig dataset or null
+   * @since 2022/05/25
+   */
+  loadDataset: async function() {
+    const datasetId = this.formDataset;
+    let dsResult = null;
+
+    dsResult = await DatasetFactory.getDataset(
+      datasetId, 
+      null, 
+      null, 
+      ['documentid',]
+    );
+
+    return dsResult;
+  },
+  
+  /**
+   * @method initDataTable
+   * @description initializes the datatable with the unfiltered data
+   * @since 2022/05/25
+   */
+  initDataTable: async function() {
+    const instanceId = this.instanceId;
+    const htmlContainer = '#dataTableContainer_' + instanceId;
+    let dsResult = null;
+
+    this.loaderInstance.show();
+
+    dsResult = await this.loadDataset();
+
+    this.loaderInstance.hide();
+
+
+    // checks if the dataset is a valid onde (it must return a "values" property)
+    if (!dsResult.values) {
+      $(htmlContainer).html(`
+        <div class="alert alert-warning">
+          ${this.getTranslation('nonexistentDataset')}
+        </div>
+      `);
+      $('#btnAddRecords_' + instanceId).attr('disabled', true);
+      $('#inputSearchRecords_' + instanceId).attr('disabled', true);
+
+      return;
+    }
+
+    // if there are no records on the dataset
+    if (dsResult.values.length == 0) {
+      $(htmlContainer).html(`
+        <div class="well text-center">
+          <img src="/style-guide/images/illustrations/empty.svg" alt="${this.getTranslation('noParamsAlt')}" title="${this.getTranslation('noParamsAlt')}" class="fs-md-margin-bottom fs-height-150"/>
+          <p><b>${this.getTranslation('noParams')}</b></p>
+        </div>
+      `);
+
+      return;
+    }
+    
+    this.datasetValues = dsResult.values;
+    this.renderTable(this.datasetValues);
+
+    return;
+  },
+
+  /**
    * @method init
    * @description method fired when the widget is loaded.
    * @since 2022/05/15
    */
   init: async function () {
-    // loads the DatasetFactory library
-    await WCMAPI.loadJS('/webdesk/vcXMLRPC.js');
-    // creates a Fluig loader instance to the widget
-    this.loaderInstance = FLUIGC.loading(
-      '#custom_params_manager_' + this.instanceId
-    );
+    try {
+      // loads the DatasetFactory library
+      await WCMAPI.loadJS('/webdesk/vcXMLRPC.js');
+      // creates a Fluig loader instance to the widget
+      this.loaderInstance = FLUIGC.loading(
+        '#custom_params_manager_' + this.instanceId
+      );
+  
+      // if the widget is in edit mode
+      if (this.isEditMode) {
+        this.loadSavedSettings();
+      } else {
+        if (this.removesTittle == 'true') {
+          $('.pageTitle').parent().remove();
+        }
 
-    // if the widget is in edit mode
-    if (this.isEditMode) {
-      this.loadSavedSettings();
-    } else {
-      if (this.removesTittle == 'true') {
-        $('.pageTitle').parent().remove();
+        this.initDataTable();
       }
+      
+    } catch (error) {
+      $("#custom_params_manager_" + this.instanceId + '_content').html(
+        '<div class="alert alert-danger">' + this.getTranslation('instanceErrorTitle') + '</div>'
+      )
+
+      FLUIGC.message.error({
+        title: this.getTranslation('instanceErrorTitle'),
+        message: this.getTranslation('instanceError'),
+        details: error
+      });
+
+      console.error(error);
     }
   },
 
